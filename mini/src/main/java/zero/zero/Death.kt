d450475fb96c78e3eb82482c3462b3d3edce59b9
thierry lachance <101730754+TLachancearduino@users.noreply.github.com>
2022-08-15 13:37:01 -0400

    Add files via upload

---
+++ mini/src/main/java/zero/zero/Death.kt
@@ -0,0 +1,34 @@
+package zero.zero//the package for this project
+
+
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
+
+
+class Death (plugin: Zero?) : Listener {
+    // the implements for the listener
+    init { //the constructor of this handler 
+        Bukkit.getPluginManager().registerEvents(this, plugin!!)
+    }
+
+    @EventHandler
+    fun onPlayerDead(e: PlayerDeathEvent) {
+        val name:String = e.player.name
+        e.player.sendMessage(name + "died an you all won this match")
+        Thread.sleep(10000)
+        Bukkit.dispatchCommand(e.player, "/server lobby")
+            //some code here
+
+        if (e.entity.killer is Player) {
+            //some code here
+            val nameKiller:String = e.player.name
+            e.player.sendMessage(nameKiller + "kill is team mate")
+            Thread.sleep(10000)
+            Bukkit.dispatchCommand(e.player, "/server lobby")
+
+        }
+    }
+}
---
+++ mini/src/main/java/zero/zero/Zero.kt
@@ -0,0 +1,16 @@
+package zero.zero
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Zero : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+        logger.info("starting")
+        PlateHandler(this) //create the plate handler
+        Death(this) //create the plate handler
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
