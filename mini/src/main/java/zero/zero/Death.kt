f2ac7cf8bd5b11c639ef9de481ada6ee154da528
thierry lachance <101730754+TLachancearduino@users.noreply.github.com>
2022-08-29 16:40:23 -0400

    save bus

--- mini/src/main/java/zero/zero/Death.kt
+++ mini/src/main/java/zero/zero/Death.kt
@@ -16,18 +16,18 @@ class Death (plugin: Mini?) : Listener {
 
     @EventHandler
     fun onPlayerDead(e: PlayerDeathEvent) {
-        val name:String = e.player.name
+        val name:String = e.player.name//name variable to store the player name
         e.player.sendMessage(name + "died an you all won this match")
-        Thread.sleep(10000)
-        Bukkit.dispatchCommand(e.player, "/server lobby")
+        Thread.sleep(10000)//waiting 10sec before tp the player
+        Bukkit.dispatchCommand(e.player, "/server lobby")//sending the player to the lobby
             //some code here
 
-        if (e.entity.killer is Player) {
+        if (e.entity.killer is Player) {//checking if the entity killer is player
             //some code here
-            val nameKiller:String = e.player.name
+            val nameKiller:String = e.player.name//namekiller saving the name of the killer to display it in the chat
             e.player.sendMessage(nameKiller + "kill is team mate")
-            Thread.sleep(10000)
-            Bukkit.dispatchCommand(e.player, "/server lobby")
+            Thread.sleep(10000)//waiting 10 sec before sending it
+            Bukkit.dispatchCommand(e.player, "/server lobby")//sending the player to the lobby
 
         }
     }
