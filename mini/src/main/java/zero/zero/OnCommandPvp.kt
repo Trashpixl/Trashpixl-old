002eccc94357a04cadd36d1b380e9d3348d9b10f
thierry lachance <101730754+TLachancearduino@users.noreply.github.com>
2022-09-13 15:48:53 -0400

    save

--- mini/src/main/java/zero/zero/OnCommandPvp.kt
+++ mini/src/main/java/zero/zero/OnCommandPvp.kt
@@ -19,18 +19,28 @@ class OnCommandPvp (plugin: Mini?) : CommandExecutor {// creating the class deat
 
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
-        var listplayer = new ArrayList<>(Bukkit.getOnlinePlayers());
-        val p = 
+        var listplayer = arrayOf(Bukkit.getOnlinePlayers())
+        
+        var p 
         val x = 0.0
         val y = 0.0
         val z = 0.0
         var w:World = p.getWorld
+        var playerCount = 1
+        for(var i = 0;i > 1;i = 0){
+            if (playerCount <= listplayer.lenght)
+            p = listplayer[playerCount]
+            //move1
+            //move2
+            else{
+                break
+            }
+        }
 
 
-
-        val to = Location(w, x, y, z)
-        var from:Location = p.getLocation()
-        PlayerTeleportEvent(p, from, to)
+        val to = Location(w, x, y, z)//move before for
+        var from:Location = p.getLocation()//move 1
+        PlayerTeleportEvent(p, from, to)//move2
 
         return true
 
