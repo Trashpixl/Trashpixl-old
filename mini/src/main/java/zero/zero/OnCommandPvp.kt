9c6e51e8797f37a769c7969ec6df23f762cece61
TLachancearduino <101730754+TLachancearduino@users.noreply.github.com>
2022-09-13 18:04:11 -0400

    bob

--- mini/src/main/java/zero/zero/OnCommandPvp.kt
+++ mini/src/main/java/zero/zero/OnCommandPvp.kt
@@ -1,10 +1,12 @@
 package zero.zero
 
+import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.World
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerTeleportEvent
 
 //the package for this project
@@ -19,28 +21,31 @@ class OnCommandPvp (plugin: Mini?) : CommandExecutor {// creating the class deat
 
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
-        var listplayer = arrayOf(Bukkit.getOnlinePlayers())
+        val listPlayer = arrayOf(Bukkit.getOnlinePlayers())
         
-        var p 
+        
         val x = 0.0
         val y = 0.0
         val z = 0.0
-        var w:World = p.getWorld
-        var playerCount = 1
-        for(var i = 0;i > 1;i = 0){
-            if (playerCount <= listplayer.lenght)
-            p = listplayer[playerCount]
-            //move1
-            //move2
-            else{
-                break
+
+        val playerCount = 1
+        while(true){
+            
+            if (playerCount <= listPlayer.size) {
+                //var p = null
+                val p:Player = listPlayer[playerCount]
+                val w: World = p.world
+                val to = Location(w, x, y, z)//move before for
+                val from: Location = p.location//move 1
+                PlayerTeleportEvent(p, from, to)//move2
+                //move1
+                //move2
             }
+            
         }
 
 
-        val to = Location(w, x, y, z)//move before for
-        var from:Location = p.getLocation()//move 1
-        PlayerTeleportEvent(p, from, to)//move2
+       
 
         return true
 
