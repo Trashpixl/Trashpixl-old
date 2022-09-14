a0f6c2de34e7e409b77c8de6609dcae379041b93
thierry lachance <101730754+TLachancearduino@users.noreply.github.com>
2022-09-14 08:46:07 -0400

    savebob

--- mini/src/main/java/zero/zero/Mini.kt
+++ mini/src/main/java/zero/zero/Mini.kt
@@ -6,7 +6,7 @@ class Mini : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
         logger.info("starting")
-       // this.getCommand("bow").setExecutor(new OnCommandPvp());
+        this.getCommand("bow").setExecutor(new OnCommandPvp());
         PlateHandler(this) //create the plate handler
         Death(this) //create the plate handler
     }
