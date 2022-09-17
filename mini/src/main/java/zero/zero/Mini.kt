fe7c0a1f040a957dd5b841e122f517772f8a413e
TLachancearduino <101730754+TLachancearduino@users.noreply.github.com>
2022-09-17 08:03:08 -0400

    save3

--- mini/src/main/java/zero/zero/Mini.kt
+++ mini/src/main/java/zero/zero/Mini.kt
@@ -6,9 +6,9 @@ class Mini : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
         logger.info("starting")
-        this.getCommand("pvp")?.setExecutor(OnCommandPvp());
-        this.getCommand("pvp")?.setExecutor(OnCommandPvpBow());
-        this.getCommand("pvp")?.setExecutor(OnCommandPvpSumo());
+        this.getCommand("pvp")?.setExecutor(OnCommandPvp())
+        this.getCommand("pvp")?.setExecutor(OnCommandPvpBow())
+        this.getCommand("pvp")?.setExecutor(OnCommandPvpSumo())
         
     }
 
