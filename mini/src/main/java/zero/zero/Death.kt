44f7387cb15017e1cb4a5bd07ffcb5ed5a562ec0
TLachancearduino <101730754+TLachancearduino@users.noreply.github.com>
2022-09-23 17:46:41 -0400

    save4

--- mini/src/main/java/zero/zero/Death.kt
+++ mini/src/main/java/zero/zero/Death.kt
@@ -1,11 +1,11 @@
 package zero.zero //the package for this project
 
 
-import org.bukkit.Bukkit //importing bukkit
-import org.bukkit.entity.Player // importing the player entitty
-import org.bukkit.event.EventHandler //importing the event handler
-import org.bukkit.event.Listener //importing the handler
-import org.bukkit.event.entity.PlayerDeathEvent //importing the player death event
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
 
 
 class Death (plugin: Zero?): Listener {// creating the class death and implementing the listener
@@ -18,16 +18,23 @@ init { //the constructor of this handler
     fun onPlayerDead(e: PlayerDeathEvent) {//creating the on player death event function and importing the death class
         val name:String = e.player.name//name variable to store the player name
         e.player.sendMessage(name + "died an you all won this match")//send the message of who won the match 
-        Thread.sleep(10000)//waiting 10sec before tp the player
+        //Thread.sleep(10000)//waiting 10sec before tp the player
         Bukkit.dispatchCommand(e.player, "/server lobby")//sending the player to the lobby
             //some code here
 
-        if (e.entity.killer is Player) {//checking if the entity killer is player
+        if (e.player.killer is Player) {//checking if the entity killer is player
             //some code here
             val nameKiller:String = e.player.name//name killer saving the name of the killer to display it in the chat
             e.player.sendMessage(nameKiller + "kill is team mate")// says who kill who 
-            Thread.sleep(10000)//waiting 10 sec before sending it
-            Bukkit.dispatchCommand(e.player, "/server lobby")//sending the player to the lobby
+            //Thread.sleep(10000)//waiting 10 sec before sending it
+            //Bukkit.dispatchCommand(e.player, "/server lobby")//sending the player to the lobby
+
+        }
+        for (p in Bukkit.getServer().onlinePlayers) {
+            //println(p.name)
+
+
+
 
         }
     }
