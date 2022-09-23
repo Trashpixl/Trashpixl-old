777dce475d0fd0a846d005428638a28f9a737fc3
TLachancearduino <101730754+TLachancearduino@users.noreply.github.com>
2022-09-23 17:11:42 -0400

    save4

--- mini/src/main/java/zero/zero/Tnt.kt
+++ mini/src/main/java/zero/zero/Tnt.kt
@@ -24,21 +24,28 @@ class Tnt : CommandExecutor {// creating the class death and implementing the li
         val x = 247.0
         val y = -58.0
         val z = 250.0
+        var count = 0
+        var playerName = ""
+
 
         for (p in getServer().onlinePlayers) {
-            println(p.name)
+            //println(p.name)
+            if (count == 0){
+                playerName = p.name().toString()
+                count++
+            }
             val w: World = p.world
             val to = Location(w, x, y, z)//move before for
             p.teleport(to)
-            p.sendMessage("3")
-            sleep(1000)
-            p.sendMessage("2")
-            sleep(1000)
-            p.sendMessage("1")
 
 
-            
         }
+        val p = getServer().getPlayer(playerName)
+        p!!.sendMessage("3")
+        sleep(1000)
+        p.sendMessage("2")
+        sleep(1000)
+        p.sendMessage("1")
 
 
 
