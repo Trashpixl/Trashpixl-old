c61160d88a3727f9f39e119f34b5549e09f5f8d5
thierry lachance <101730754+TLachancearduino@users.noreply.github.com>
2022-09-29 11:19:17 -0400

    save

--- mini/src/main/java/zero/zero/handler/Loot.kt
+++ mini/src/main/java/zero/zero/handler/Loot.kt
@@ -20,7 +20,7 @@ class Loot (plugin: Zero?) : Listener {
         if (ev.action == Action.PHYSICAL) { //check if the action is physical
             if (ev.clickedBlock!!.type == Material.STONE_BUTTON_PLATE) { //compare what the player sept on to a stone pressure plate and is required a non-nullable
                 val p = ev.getName()
-//here boby
+//here boby bob
             
         }
                 }
