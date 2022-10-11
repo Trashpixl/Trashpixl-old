8bf4b00a46edf4d7a90bff47ffbd219de4cc2da5
TLachancearduino <101730754+TLachancearduino@users.noreply.github.com>
2022-10-11 18:16:42 -0400

    save5

--- mini/src/main/java/zero/zero/handler/Loot.kt
+++ mini/src/main/java/zero/zero/handler/Loot.kt
@@ -17,59 +17,58 @@ class Loot (plugin: Zero?) : Listener {
     init { //the constructor of this handler 
         Bukkit.getPluginManager().registerEvents(this, plugin!!)
     }
-var p: Player? = null
+
 
     @EventHandler
     fun buttonHandler(ev: PlayerInteractEvent) { //describes what the event is
         if (ev.action == Action.PHYSICAL) { //check if the action is physical
             if (ev.clickedBlock!!.type == Material.STONE_BUTTON) { //compare what the player sept on to a stone pressure plate and is required a non-nullable
-                p = ev.player
-                val itemtest = ItemStack(Material.LEATHER_HELMET, 1)
-                p!!.inventory.setItem((1..36).random(), itemtest)
+                val p = ev.player
+
 
                 val kit = (1..6).random()
                 if(kit == 1){
                     val item1 = ItemStack(Material.IRON_SWORD, 1)
                     val item2 = ItemStack(Material.GOLDEN_APPLE, (1..64).random())
-                    p!!.inventory.setItem((1..36).random(), item1)
-                    p!!.inventory.setItem((1..36).random(), item2)
+                    p.inventory.setItem((1..36).random(), item1)
+                    p.inventory.setItem((1..36).random(), item2)
 
                 }
                 else{
                     if(kit == 2){
                         val item1 = ItemStack(Material.DIAMOND_CHESTPLATE, 1)
                         val item2 = ItemStack(Material.BREAD, (1..64).random())
-                        p!!.inventory.setItem((1..36).random(), item1)
-                        p!!.inventory.setItem((1..36).random(), item2)
+                        p.inventory.setItem((1..36).random(), item1)
+                        p.inventory.setItem((1..36).random(), item2)
 
                     }
                     else{
                         if(kit == 3){
                             val item1 = ItemStack(Material.WOODEN_AXE)
                             val item2 = ItemStack(Material.LEATHER_BOOTS)
-                            p!!.inventory.setItem((1..36).random(), item1)
-                            p!!.inventory.setItem((1..36).random(), item2)
+                            p.inventory.setItem((1..36).random(), item1)
+                            p.inventory.setItem((1..36).random(), item2)
                         }
                         else{
                             if(kit == 4){
                                 val item1 = ItemStack(Material.GOLDEN_CHESTPLATE)
                                 val item2 = ItemStack(Material.WOODEN_SWORD)
-                                p!!.inventory.setItem((1..36).random(), item1)
-                                p!!.inventory.setItem((1..36).random(), item2)
+                                p.inventory.setItem((1..36).random(), item1)
+                                p.inventory.setItem((1..36).random(), item2)
                             }
                             else{
                                 if(kit == 5){
                                     val item1 = ItemStack(Material.IRON_BOOTS)
                                     val item2 = ItemStack(Material.LEATHER_HELMET)
-                                    p!!.inventory.setItem((1..36).random(), item1)
-                                    p!!.inventory.setItem((1..36).random(), item2)
+                                    p.inventory.setItem((1..36).random(), item1)
+                                    p.inventory.setItem((1..36).random(), item2)
                                 }
                                 else{
                                 if(kit == 6){
                                     val item1 = ItemStack(Material.CHAINMAIL_LEGGINGS)
                                     val item2 = ItemStack(Material.STICK)
-                                    p!!.inventory.setItem((1..36).random(), item1)
-                                    p!!.inventory.setItem((1..36).random(), item2)
+                                    p.inventory.setItem((1..36).random(), item1)
+                                    p.inventory.setItem((1..36).random(), item2)
                                 }
                                     else{
                                     print("ew")
