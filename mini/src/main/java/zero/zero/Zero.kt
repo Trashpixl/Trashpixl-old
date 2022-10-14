cc81726cbb2f58e2a882089130a19d38aefdd36e
TLachancearduino <101730754+TLachancearduino@users.noreply.github.com>
2022-10-14 16:10:39 -0400

    save

--- mini/src/main/java/zero/zero/Zero.kt
+++ mini/src/main/java/zero/zero/Zero.kt
@@ -5,13 +5,13 @@ import zero.zero.command.*
 import zero.zero.handler.Death
 import zero.zero.handler.Loot
 import zero.zero.handler.RaceTimer
+import java.io.ObjectInputFilter
 
 class Zero : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
         //plugin.saveDefaultConfig()
         //Config.load("config.yml")
-
         Death(this)
         RaceTimer(this)
         Loot(this)
@@ -25,11 +25,26 @@ class Zero : JavaPlugin() {
         this.getCommand("onCommandPvpLoot")?.setExecutor(OnCommandPvpLoot())
         this.getCommand("onCommandPvpSumo")?.setExecutor(OnCommandPvpSumo())
         this.getCommand("onCommandParkour")?.setExecutor(OnCommandParkour())
-        this.getCommand("onCommandParkour")?.setExecutor(OnCommandParkour())
+        this.getCommand("onCommandCaptureFlag")?.setExecutor(OnCommandCaptureFlag())
+        this.getCommand("onCommandChaiseMusical")?.setExecutor(OnCommandChaiseMusical())
+        this.getCommand("onCommandClimbFast")?.setExecutor(OnCommandClimbFast())
+        this.getCommand("onCommandCpsTest")?.setExecutor(OnCommandCpsTest())
+        this.getCommand("onCommandDodgeBall")?.setExecutor(OnCommandDodgeBall())
+        this.getCommand("onCommandElytraRace")?.setExecutor(OnCommandElytraRace())
+        this.getCommand("onCommandFallTrap")?.setExecutor(OnCommandFallTrap())
+        this.getCommand("onCommandFindMe")?.setExecutor(OnCommandFindMe())
+        this.getCommand("onCommandFireTrap")?.setExecutor(OnCommandFireTrap())
+        this.getCommand("onCommandHideAndSeek")?.setExecutor(OnCommandHideAndSeek())
+        this.getCommand("onCommandHorseRace")?.setExecutor(OnCommandHorseRace())
+        this.getCommand("onCommandMlg")?.setExecutor(OnCommandMlg())
+        this.getCommand("onCommandRockPaperCisor")?.setExecutor(OnCommandRockPaperCisor())
+        this.getCommand("onCommandSkywars")?.setExecutor(OnCommandSkywars())
+        this.getCommand("onCommandSwimFast")?.setExecutor(OnCommandSwimFast())
+        this.getCommand("onCommandTag")?.setExecutor(OnCommandTag())
         //add more here
     }
    public fun write(write:String, where:String){
-       // plugin.getConfig().set(where, write)
+       //plugin.getConfig().set(where, write)
     }
    public fun read(toRead:String){
         //val content:String = plugin.getConfig().getString(toRead)
