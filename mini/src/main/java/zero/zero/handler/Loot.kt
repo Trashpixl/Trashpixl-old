b2c437006bafed8b51c652cc77d045ddd7f5b772
TLachancearduino <101730754+TLachancearduino@users.noreply.github.com>
2022-10-22 16:31:08 -0400

    save

--- mini/src/main/java/zero/zero/handler/Loot.kt
+++ mini/src/main/java/zero/zero/handler/Loot.kt
@@ -10,6 +10,10 @@ import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
 import zero.zero.Zero
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+import java.util.*
 
 
 class Loot (plugin: Zero?) : Listener {
@@ -23,74 +27,85 @@ class Loot (plugin: Zero?) : Listener {
     fun buttonHandler(ev: PlayerInteractEvent) { //describes what the event is
         if (ev.action == Action.LEFT_CLICK_BLOCK) { //check if the action is physical
             if (ev.clickedBlock!!.type == Material.STONE_BUTTON) { //compare what the player sept on to a stone pressure plate and is required a non-nullable
-             val p = ev.player
+                val fileName = "game.txt"// creating the file name
+                val toWrite = "0"
+                val actualFile = File(fileName)
+                if(actualFile.exists() && actualFile.isFile){
+                    val reader = Scanner(actualFile)
+                    val data = reader.nextLine()
+                    val dataInt = data.toInt()
+                    if(dataInt == 2){
 
-              if(p.location.world.name.endsWith("overworld")){
+                        val p = ev.player
+
+                        if(p.location.world.name.endsWith("overworld")){
 
 
 
-                val kit = (1..6).random()
-                if(kit == 1){
-                    val item1 = ItemStack(Material.IRON_SWORD, 1)
-                    val item2 = ItemStack(Material.GOLDEN_APPLE, (1..64).random())
-                    p.inventory.setItem((1..36).random(), item1)
-                    p.inventory.setItem((1..36).random(), item2)
-
-                }
-                else{
-                    if(kit == 2){
-                        val item1 = ItemStack(Material.DIAMOND_CHESTPLATE, 1)
-                        val item2 = ItemStack(Material.BREAD, (1..64).random())
-                        p.inventory.setItem((1..36).random(), item1)
-                        p.inventory.setItem((1..36).random(), item2)
-
-                    }
-                    else{
-                        if(kit == 3){
-                            val item1 = ItemStack(Material.WOODEN_AXE)
-                            val item2 = ItemStack(Material.LEATHER_BOOTS)
-                            p.inventory.setItem((1..36).random(), item1)
-                            p.inventory.setItem((1..36).random(), item2)
-                        }
-                        else{
-                            if(kit == 4){
-                                val item1 = ItemStack(Material.GOLDEN_CHESTPLATE)
-                                val item2 = ItemStack(Material.WOODEN_SWORD)
+                            val kit = (1..6).random()
+                            if(kit == 1){
+                                val item1 = ItemStack(Material.IRON_SWORD, 1)
+                                val item2 = ItemStack(Material.GOLDEN_APPLE, (1..64).random())
                                 p.inventory.setItem((1..36).random(), item1)
                                 p.inventory.setItem((1..36).random(), item2)
+
                             }
                             else{
-                                if(kit == 5){
-                                    val item1 = ItemStack(Material.IRON_BOOTS)
-                                    val item2 = ItemStack(Material.LEATHER_HELMET)
+                                if(kit == 2){
+                                    val item1 = ItemStack(Material.DIAMOND_CHESTPLATE, 1)
+                                    val item2 = ItemStack(Material.BREAD, (1..64).random())
                                     p.inventory.setItem((1..36).random(), item1)
                                     p.inventory.setItem((1..36).random(), item2)
+
                                 }
                                 else{
-                                if(kit == 6){
-                                    val item1 = ItemStack(Material.CHAINMAIL_LEGGINGS)
-                                    val item2 = ItemStack(Material.STICK)
-                                    p.inventory.setItem((1..36).random(), item1)
-                                    p.inventory.setItem((1..36).random(), item2)
-                                }
+                                    if(kit == 3){
+                                        val item1 = ItemStack(Material.WOODEN_AXE)
+                                        val item2 = ItemStack(Material.LEATHER_BOOTS)
+                                        p.inventory.setItem((1..36).random(), item1)
+                                        p.inventory.setItem((1..36).random(), item2)
+                                    }
                                     else{
-                                    
-                                }
+                                        if(kit == 4){
+                                            val item1 = ItemStack(Material.GOLDEN_CHESTPLATE)
+                                            val item2 = ItemStack(Material.WOODEN_SWORD)
+                                            p.inventory.setItem((1..36).random(), item1)
+                                            p.inventory.setItem((1..36).random(), item2)
+                                        }
+                                        else{
+                                            if(kit == 5){
+                                                val item1 = ItemStack(Material.IRON_BOOTS)
+                                                val item2 = ItemStack(Material.LEATHER_HELMET)
+                                                p.inventory.setItem((1..36).random(), item1)
+                                                p.inventory.setItem((1..36).random(), item2)
+                                            }
+                                            else{
+                                                if(kit == 6){
+                                                    val item1 = ItemStack(Material.CHAINMAIL_LEGGINGS)
+                                                    val item2 = ItemStack(Material.STICK)
+                                                    p.inventory.setItem((1..36).random(), item1)
+                                                    p.inventory.setItem((1..36).random(), item2)
+                                                }
+                                                else{
+
+                                                }
+                                            }
+                                        }
+                                    }
                                 }
                             }
                         }
+                        try{
+                            val myWriter = FileWriter(actualFile)//pointing the writer to the actual file
+                            myWriter.write(toWrite)//writing the data to the file
+                            myWriter.close()//closing the writer
+                        }
+                        catch(e: IOException){
+                            throw RuntimeException(e)
+                        }
                     }
                 }
-
-
-
+            }
         }
-                }
-        }
-        }
-
-
-
-
-
     }
+}
--- mini/src/main/java/zero/zero/handler/RaceTimer.kt
+++ mini/src/main/java/zero/zero/handler/RaceTimer.kt
@@ -11,6 +11,10 @@ import org.bukkit.event.Listener//import all the listener
 import org.bukkit.event.block.Action//import the action related to the block
 import org.bukkit.event.player.PlayerInteractEvent//import the action related to the player
 import zero.zero.Zero// importing the main class
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+import java.util.*
 
 
 class RaceTimer (plugin: Zero?) : Listener {
@@ -23,27 +27,44 @@ class RaceTimer (plugin: Zero?) : Listener {
     fun pressurePlateHandler(ev: PlayerInteractEvent) { //describes what the event is
         if (ev.action == Action.PHYSICAL) { //check if the action is physical
             if (ev.clickedBlock!!.type == Material.STONE_PRESSURE_PLATE) { //compare what the player sept on to a stone pressure plate and is required a non-nullable
-                val x = 0.0// creating the x variable for player tp
-                val y = 0.0// creating the y variable for player tp
-                val z = 0.0// creating the z variable for player tp
-                val p = ev.player //create the local player id
-                if(p.location.world.name.endsWith("overworld")){//checking in wich environment the player is
-                val name = p.name//test cccalebbbooooubbbouuu
+                val fileName = "game.txt"// creating the file name
+                val toWrite = "0"
+                val actualFile = File(fileName)
+                if(actualFile.exists() && actualFile.isFile){
+                    val reader = Scanner(actualFile)
+                    val data = reader.nextLine()
+                    val dataInt = data.toInt()
+                    if(dataInt == 3){
+                        val x = 0.0// creating the x variable for player tp
+                        val y = 0.0// creating the y variable for player tp
+                        val z = 0.0// creating the z variable for player tp
+                        val p = ev.player //create the local player id
+                        if(p.location.world.name.endsWith("overworld")){//checking in wich environment the player is
+                            val name = p.name//test cccalebbbooooubbbouuu
 
-                p.sendMessage(name + "won the race")//bob
-                for (player in getServer().onlinePlayers) {// listing all the player in the server
-                    val w: World = player.world// creating a variable to store the player world
-                    val to = Location(w, x, y, z)//move before for
-                    player.teleport(to)// teleporting the palyer
+                            p.sendMessage(name + "won the race")//bob
+                            for (player in getServer().onlinePlayers) {// listing all the player in the server
+                                val w: World = player.world// creating a variable to store the player world
+                                val to = Location(w, x, y, z)//move before for
+                                player.teleport(to)// teleporting the palyer
 
 
 
-        }
+                            }
+                        }
+
+                        try{
+                            val myWriter = FileWriter(actualFile)//pointing the writer to the actual file
+                            myWriter.write(toWrite)//writing the data to the file
+                            myWriter.close()//closing the writer
+                        }
+                        catch(e: IOException){
+                            throw RuntimeException(e)
+                        }
+
+
+                    }
                 }
-
-
-
-
             }
         }
     }
