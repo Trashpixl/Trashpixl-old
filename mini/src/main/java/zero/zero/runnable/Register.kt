c88bd3da267cd89b91f1b1553e13a717d0e38373
thierry lachance <101730754+TLachancearduino@users.noreply.github.com>
2022-11-03 15:10:37 -0400

    Save Register.kt.

--- mini/src/main/java/zero/zero/runnable/Register.kt
+++ mini/src/main/java/zero/zero/runnable/Register.kt
@@ -1,9 +1,15 @@
 package zero.zero.runnable
 
 class Register {
-fun Put(){
-    //function to send packet to data base
-}
 
+    fun Put(){
+    //function to send packet to database
+    }
+    fun Get(){
+    //function to get something from the database
+    }
+    fun Erase(){
+    //erase somthing from the database
+    }
 
 }
