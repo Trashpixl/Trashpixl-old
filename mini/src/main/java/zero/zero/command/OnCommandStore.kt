3a221426d9b0267f426a792d1cf5feb80fc78217
thier <lachancethierry6@gmail.com>
2022-11-13 09:27:44 -0500

    save

--- mini/src/main/java/zero/zero/command/OnCommandStore.kt
+++ mini/src/main/java/zero/zero/command/OnCommandStore.kt
@@ -20,14 +20,22 @@ class OnCommandStore: CommandExecutor {// creating the class death and implement
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
         for(p in getServer().onlinePlayers){
             if(p.name == sender.name){
+                var item0:ItemStack = ItemStack(Material.BARRIER, 1)
                 var item1:ItemStack = ItemStack(Material.IRON_SWORD, 1)
                 var item2:ItemStack = ItemStack(Material.APPLE, 1)
                 var item3:ItemStack = ItemStack(Material.OAK_WOOD, 1)
                 var item4:ItemStack = ItemStack(Material.REDSTONE, 1)
+                var item5:ItemStack = ItemStack(Material.IRON_CHESTPLATE, 1)
+                var item6:ItemStack = ItemStack(Material.RED_TULIP, 1)
+                var item7:ItemStack = ItemStack(Material.BUCKET, 1)
+                p.inventory.setItem(0, item0)
                 p.inventory.setItem(1, item1)
                 p.inventory.setItem(2, item2)
                 p.inventory.setItem(3, item3)
                 p.inventory.setItem(4, item4)
+                p.inventory.setItem(5, item5)
+                p.inventory.setItem(6, item6)
+                p.inventory.setItem(7, item7)
                 
 
 
